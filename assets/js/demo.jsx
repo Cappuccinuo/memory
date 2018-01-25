3ca017b05af09447123eed33c709e7ffb1da1860
Yuan Gao <gaoyuanbj@yahoo.com>
2018-01-25 17:27:23 -0500

    Finish the basic structure of memory game

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -1,3 +1,5 @@
+'use strict'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
@@ -6,44 +8,131 @@ import { Button } from 'reactstrap';
 const uniqueCards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 export default function run_demo(root) {
-  ReactDOM.render(<Demo />, root);
+  ReactDOM.render(<Board/>, root);
 }
 
-class Demo extends React.Component {
+class Square extends React.Component {
+  render() {
+    const toggleVisible = this.props.isTurned ? 'visible' : this.props.isMatch ? 'visible' : 'hidden';
+    const changeColor = this.props.isMatch ? '#C4C9CF' : 'white';
+    let style = {
+      visibility: toggleVisible,
+    };
+    let wholeStyle = {
+      background: changeColor,
+    }
+    return (
+      <button className="square" style={wholeStyle} onClick={this.props.onClick}>
+        <div style={style}>
+          <div>{this.props.value}</div>
+        </div>
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
   constructor(props) {
     super(props);
+    this.restart = this.restart.bind(this);
+    this.resetTime = null;
     this.state = this.initialState();
   }
 
   initialState() {
     return {
-      cards: [],
-      moves: 0,
+      squares: this.shuffleDeck(),
       selected: [],
-    }
-  }
-
-  gameBoard() {
-    return (
-      <div id="gameBoard">
-        Hello Memory
-      </div>
-    )
+      pairs: [],
+    };
   }
 
   restart() {
     this.setState(this.initialState());
   }
 
+  shuffleDeck() {
+    let deck = uniqueCards.slice();
+    deck = deck.concat(deck);
+    deck = _.shuffle(deck);
+    return deck;
+  }
+
+  handleClick(i) {
+    if (this.state.selected.includes(i) || this.resetTime) {
+      return;
+    }
+    if (this.state.selected.length >= 1) {
+      this.resetTime = setTimeout(() => {
+        this.checkMatch();
+      }, 500);
+    }
+
+    let deck = this.state.squares;
+    const selected = this.state.selected.slice();
+    const squares = this.state.squares.slice();
+    squares[i] = deck[i];
+    this.state.selected.push(i);
+    this.setState({selected: this.state.selected});
+    this.setState({squares: squares});
+  }
+
+  checkMatch() {
+    let pairs = this.state.pairs;
+    const matchSelected = this.state.selected.map((i) => {
+      return this.state.squares[i];
+    });
+
+    if (matchSelected[0] == matchSelected[1]) {
+      pairs = pairs.concat(this.state.selected);
+    }
+
+    this.setState({selected: [], pairs: pairs});
+    this.resetTime = null;
+
+    if (this.state.pairs.length === this.state.squares.length) {
+      alert("win");
+    }
+  }
+
+  renderSquare(i) {
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}/>;
+  }
+
+  gameBoard() {
+    return (
+      <div id="gameBoard">
+        {this.state.squares.map((card, i) => {
+          return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+            isTurned={this.state.selected.includes(i)}
+            isMatch={this.state.pairs.includes(i)}
+            key={i}/>;
+        }, this)}
+      </div>
+    )
+  }
+
   render() {
+    const score = "Score: ";
     const gameboard = this.gameBoard();
+
     return (
       <div>
-        <div className="endMsg"></div>
-        <div className="score">
-          <span>Score:  {this.state.moves}</span>
+        <div className="restart">
+          <button onClick={this.restart}>Restart</button>
         </div>
-        {gameboard}
+        <div className="game">
+          <div className="game-board">
+            {gameboard}
+          </div>
+          <div className="game-info">
+            <div>{/* score */}</div>
+          </div>
+        </div>
       </div>
     );
   }
